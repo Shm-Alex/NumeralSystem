b3197205685c05ecbbfc96a5b418357565d69ced
shmykov.alexander <shmykov.alexander@gmail.com>
2025-04-16 17:05:25 +0300

    Add classlib

---
+++ NumeralSystem/NumeralSystem/NumeralSystem.cs
@@ -0,0 +1,27 @@
+﻿namespace NumeralSystem
+{
+    public class NumeralSystem
+    {
+        private readonly char[] digits;
+
+        public NumeralSystem(char[] alphaBit)
+        {
+            this.digits = alphaBit;
+            if (alphaBit == null || alphaBit.Length == 0) throw new ArgumentNullException(nameof(alphaBit));
+
+        }
+        public char[] ToDigits(uint n)
+        {
+            List<char> result = new List<char>();
+            uint radix = (uint)digits.Length;
+            while (!(n < digits.Length))
+            {
+                var indx = n % (radix);
+                result.Add(digits[indx]);
+                n /= radix;
+            }
+            result.Add(digits[n]);
+            return result.ToArray();
+        }
+    }
+}
